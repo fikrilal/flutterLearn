f481e1aa8086ffbe481d327cc886a8d237771b09
fikrilal <fikrimuzakky24@gmail.com>
2023-04-10 07:29:43 +0700

    remove

--- lib/list.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: application/json');
-include "../flutter_api/db.php";
-$stmt = $db->prepare("SELECT id, name, email FROM users");
-$stmt->execute();
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo json_encode($result);
-?>
